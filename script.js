18041ff5fe5a01383df48ecbe79540b787b6cb8c
Matt Zachman <mzachman14@gmail.com>
2022-12-16 02:05:05 -0500

    Allow user interaction with display

--- script.js
+++ script.js
@@ -1,38 +1,61 @@
-const playerFac = () => {
-    const sayHello = () => console.log('hello!');
-    return { sayHello };
+const playerFac = (str) => {
+    const playerSymbol = str;
+    return { playerSymbol };
 }
 
 const gameboard = (() => {
 
-    const playerOne = playerFac();
-    const playerTwo = playerFac();
-
+    const playerOne = playerFac('X');
+    const playerTwo = playerFac('O');
+    let numMoves = 0;
     const gameArr = [
-        ['X', 'O', 'X'],
-        ['O', 'X', 'O'],
-        ['X', 'O', 'X'],
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
     ]
-    return { playerOne, playerTwo, gameArr };
+
+    const click = (row, col) => {
+        if(numMoves % 2 === 0){
+            gameArr[row][col] = playerOne.playerSymbol;
+        }else{
+            gameArr[row][col] = playerTwo.playerSymbol;
+        }
+        numMoves += 1;
+    }
+
+    return { playerOne, playerTwo, gameArr, click };
 })();
 
 const display = (() => {
 
     const container = document.querySelector('#container');
 
-    const addCell = (str) => {
+    const addCell = (str, row, col) => {
         const cell = document.createElement('div');
         cell.textContent = str;
         cell.classList.add('cell');
+        cell.setAttribute('row', row);
+        cell.setAttribute('col', col);
         container.appendChild(cell);
     }
 
     const showGameState = (arr) => {
+        while(container.firstChild){
+            container.removeChild(container.firstChild);
+        }
          for(let i = 0; i < arr.length; i+=1){
             for(let j = 0; j < arr.length; j+=1){
-                addCell(arr[i][j]);
+                addCell(arr[i][j], i, j);
             }
          }
+
+         const cells = document.querySelectorAll('.cell');
+         cells.forEach((div) => {
+            div.addEventListener('click', () => {
+                gameboard.click(div.getAttribute('row'),div.getAttribute('col'));
+                showGameState(gameboard.gameArr);
+            })
+         })
     }
 
     return { showGameState };
@@ -40,7 +63,4 @@ const display = (() => {
 
 
 
-
-gameboard.playerOne.sayHello();
-gameboard.playerTwo.sayHello();
 display.showGameState(gameboard.gameArr);
