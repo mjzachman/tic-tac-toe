54f9cdfe0a567e8623d146f9427078e4aa79436a
Matt Zachman <mzachman14@gmail.com>
2022-12-19 16:12:22 -0500

    Write win logic

--- script.js
+++ script.js
@@ -15,6 +15,51 @@ const game = (() => {
     ["", "", ""],
   ];
 
+  const checkRows = () => {
+    for(let i = 0; i < gameArr.length; i += 1){
+        if(gameArr[i].every(element => element === playerOne.symbol)){
+            playerOne.win = true;
+        } else if(gameArr[i].every(element => element === playerTwo.symbol)){
+            playerTwo.win = true;
+        }
+    }
+  }
+
+  const checkCols = () => {
+    for ( let i = 0; i < gameArr[0].length; i += 1){
+        const currentCol = [];
+
+        for (let j = 0; j < gameArr.length; j += 1){
+            currentCol.push(gameArr[j][i]);
+        }
+
+        if(currentCol.every(element => element === playerOne.symbol)){
+            playerOne.win = true;
+        } else if(currentCol.every(element => element === playerTwo.symbol)){
+            playerTwo.win = true;
+        }
+    }
+  }
+
+  const checkDiags = () => {
+    const diag1 = [gameArr[0][0], gameArr[1][1], gameArr[2][2]];
+    const diag2 = [gameArr[0][2], gameArr[1][1], gameArr[2][0]];
+
+    if(diag1.every(element => element === playerOne.symbol)){
+        playerOne.win = true;
+    } else if (diag1.every(element => element === playerTwo.symbol)){
+        playerTwo.win = true;
+    }
+
+    if(diag2.every(element => element === playerOne.symbol)){
+        playerOne.win = true;
+    } else if (diag2.every(element => element === playerTwo.symbol)) {
+        playerTwo.win = true;
+    }
+
+
+  }
+
   const click = (row, col) => {
     if (gameArr[row][col] !== "") {
       return;
@@ -27,9 +72,9 @@ const game = (() => {
     }
     numMoves += 1;
 
-    if (numMoves === 9) {
-      playerOne.win = true;
-    }
+    checkRows();
+    checkCols();
+    checkDiags();
   };
 
   return { playerOne, playerTwo, gameArr, click };
@@ -38,9 +83,9 @@ const game = (() => {
 const display = (() => {
   const container = document.querySelector("#container");
 
-  const addCell = (str, row, col) => {
+  const addCell = (row, col) => {
     const cell = document.createElement("div");
-    cell.textContent = str;
+    cell.textContent = game.gameArr[row][col];
     cell.classList.add("cell");
     cell.setAttribute("row", row);
     cell.setAttribute("col", col);
@@ -61,7 +106,7 @@ const display = (() => {
     }
     for (let i = 0; i < game.gameArr.length; i += 1) {
       for (let j = 0; j < game.gameArr.length; j += 1) {
-        addCell(game.gameArr[i][j], i, j);
+        addCell(i, j);
       }
     }
 
